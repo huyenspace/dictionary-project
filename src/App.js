19447d1ffc592e644bb0e4cde050e5123b255ecf
huyenspace <huyen.space99@gmail.com>
2021-12-07 21:10:22 +0700

    Retouch

--- src/App.js
+++ src/App.js
@@ -6,7 +6,26 @@ export default function App() {
     <div className="App">
       <h1>⟬ Dictionary ⟭</h1>
       <p className="intro">What word would you like to search?</p>
-      <Dictionary />
+      <Dictionary defaultKeyword="money" />
+      <hr />
+      <footer>
+        {" "}
+        <a
+          href="https://github.com/huyenspace/dictionary-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source
+        </a>{" "}
+        code by{" "}
+        <a
+          href="https://github.com/huyenspace/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Huyen Nguyen
+        </a>
+      </footer>
     </div>
   );
 }
