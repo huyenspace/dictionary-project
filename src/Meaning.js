d285a8442a8d1949d0e2acce87bc550786a028c6
huyenspace <huyen.space99@gmail.com>
2021-12-07 19:30:15 +0700

    Add photo

--- src/Meaning.js
+++ src/Meaning.js
@@ -6,23 +6,19 @@ export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div className="Meaning">
-      <div className="row">
-        <div className="col-7">
-          <h3>{props.meaning.partOfSpeech}</h3>
-          {props.meaning.definitions.map(function (definition, index) {
-            return (
-              <div key={index}>
-                <p>
-                  » {definition.definition}
-                  <br />
-                  <em>⁍ {definition.example}</em>
-                  <Synonyms synonyms={definition.synonyms} />
-                </p>
-              </div>
-            );
-          })}
-        </div>
-      </div>
+      <h3>{props.meaning.partOfSpeech}</h3>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p>
+              » {definition.definition}
+              <br />
+              <em>⁍ {definition.example}</em>
+              <Synonyms synonyms={definition.synonyms} />
+            </p>
+          </div>
+        );
+      })}
     </div>
   );
 }
